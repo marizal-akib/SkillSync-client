9049ef89e8fb61e0b1266870bacf5119f710624b
marizal-akib <marizalakib@gmail.com>
2023-11-06 01:41:10 +0000

    Implement the 404 page with a back-to-home button

--- src/components/pages/Error/Error.jsx
+++ src/components/pages/Error/Error.jsx
@@ -1,11 +1,23 @@
-
+import { Link } from "react-router-dom";
 
 const Error = () => {
-    return (
-        <div>
-            <h2>Error</h2>
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://i.ibb.co/MnvY7mV/webmaster-day-connection-error-404-abstract-illustration-connection-error-715074-200.jpg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-10"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          
+          <Link to="/" className="btn btn-primary mt-80 lg:mt-96">Back to Home</Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Error;
