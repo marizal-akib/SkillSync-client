93f4311af2382c61d0bfd1248065054edb6b2b96
marizal-akib <marizalakib@gmail.com>
2023-11-09 05:27:43 +0000

    sending bid request to server

--- src/components/pages/BidPage/BidPage.jsx
+++ src/components/pages/BidPage/BidPage.jsx
@@ -1,5 +1,6 @@
 import { Link, useLoaderData } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const BidPage = () => {
   const jobData = useLoaderData();
@@ -19,16 +20,58 @@ const BidPage = () => {
     maxPrice,
   } = job;
 
+  const handelBid = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const jobId = _id ;
+    const bidderEmail = user.email;
+    const offerDeadline = form.deadline.value;
+    const offerPrice = form.offerPricePrice.value;
+
+    const jobBid = {
+      jobTitle,
+      jobId,
+      employerEmail,
+      bidderEmail,
+      offerPrice,
+      offerDeadline,
+    };
+    console.log(jobBid);
+    fetch("http://localhost:5000/bid", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(jobBid),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          console.log("success");
+          form.reset();
+          Swal.fire({
+            title: "Requested!",
+            text: "Job requested successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
+
   return (
     <div className="flex md:grid md:grid-cols-3  flex-col-reverse ">
-      <div className="bg-[#f4f3f0] py-4 md:p-20 px-2 col-span-1 md:col-span-2 ">
+      <div className="bg-[#f4f3f0] py-4 md:p-20 px-10 col-span-1 md:col-span-2 ">
         <h2 className="text-3xl text-left mb-6 font-extrabold space-x-3">
           Place Your Bid
         </h2>
-        <form className="md:max-w-3xl mx-auto" action="">
+        <form className="md:max-w-3xl mx-auto" onSubmit={handelBid} action="">
           <div className="form-control md:w-full">
             <label className="label">
-              <span className="label-text text-lg font-bold">Email</span>
+              <span className="label-text text-xl font-bold">Email:</span>
             </label>
             <label className="input-group">
               <input
@@ -36,24 +79,24 @@ const BidPage = () => {
                 name="email"
                 value={user?.email}
                 placeholder="Email"
-                className=" w-full bg-transparent text-xl font-semibold active:border-none border-none "
+                className=" w-full bg-transparent text-lg font-semibold active:border-none border-none "
                 readOnly
               />
             </label>
             {user?.email !== employerEmail && (
               <>
                 <label className="label">
-                  <span className="label-text text-lg font-bold">
-                    Employer Email
+                  <span className="label-text text-xl font-bold">
+                    Employer Email:
                   </span>
                 </label>
                 <label className="input-group">
                   <input
                     type="text"
-                    name="email"
+                    name="buyerEmail"
                     value={employerEmail}
                     placeholder="Email"
-                    className=" w-full bg-transparent text-xl font-semibold active:border-none border-none "
+                    className=" w-full bg-transparent text-lg font-semibold active:border-none border-none "
                     readOnly
                   />
                 </label>
@@ -70,8 +113,8 @@ const BidPage = () => {
               <label className="input-group mb-3 align-middle">
                 <input
                   type="text"
-                  name="minPrice"
-                  placeholder="Min Price"
+                  name="offerPrice"
+                  placeholder="Offer Your Price"
                   defaultValue={minPrice}
                   className="input  input-bordered"
                 />
@@ -88,16 +131,16 @@ const BidPage = () => {
           </div>
           <div className="text-end">
             <input
-              className='btn btn-success rounded-md text-blue-900 '
+              className="btn btn-success rounded-md text-blue-900 "
               disabled={user?.email === employerEmail}
               type="submit"
-              value="Update job"
+              value="Bid on the project"
             />
           </div>
         </form>
       </div>
-      <div className="border bg-[#f4f3f0] p-4  ">
-        <h2 className="text-lg text-left md:mt-12 font-semibold ">
+      <div className="border bg-[#f4f3f0] md:px-10 p-4  ">
+        <h2 className="text-xl text-left md:mt-12 font-semibold ">
           Job details :
         </h2>
         <div className="divider"></div>
